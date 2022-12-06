8bcc58e44394104e1ef124e2c8720ef2125a5ace
mdanys <mochammaddany@gmail.com>
2022-12-07 02:23:54 +0700

    setup config, echo, and mysql

--- utils/database/mysql.go
+++ utils/database/mysql.go
@@ -1,1 +1,33 @@
 package database
+
+import (
+	"GunungKuy/config"
+	"fmt"
+
+	"github.com/labstack/gommon/log"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB(c *config.AppConfig) *gorm.DB {
+	str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.DBUser,
+		c.DBPass,
+		c.DBHost,
+		c.DBPort,
+		c.DBName,
+	)
+
+	db, err := gorm.Open(mysql.Open(str), &gorm.Config{})
+	if err != nil {
+		log.Error("db config error: ", err.Error())
+		return nil
+	}
+
+	return db
+}
+
+// FUNC TO MIGRATE TABLE TO DATABASE
+func MigrateDB(db *gorm.DB) {
+	db.AutoMigrate()
+}
