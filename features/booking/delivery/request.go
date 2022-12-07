de67f863ba0643060f1dbede24d1991492cd34ad
mdanys <mochammaddany@gmail.com>
2022-12-07 08:41:57 +0700

    add request.go, response.go, handler.go and oauth google

--- features/booking/delivery/request.go
+++ features/booking/delivery/request.go
@@ -1,1 +1,96 @@
 package delivery
+
+import (
+	"GunungKuy/features/booking/domain"
+	"time"
+)
+
+type RegisterFormat struct {
+	IdUser        uint             `json:"id_user" form:"id_user"`
+	Start         string           `json:"date_start" form:"date_start  validate:"required""`
+	End           string           `json:"date_end" form:"date_end  validate:"required""`
+	Entrance      string           `json:"entrance" form:"entrance" validate:"required"`
+	Ticket        int              `json:"ticket" form:"ticket" validate:"required"`
+	OrderId       string           `json:"order_id" form:"order_id"`
+	Product       []BookingProduct `json:"product" form:"product"`
+	IdRanger      uint             `json:"id_ranger" form:"id_ranger"`
+	StatusBooking string           `json:"status_booking" form:"status_booking"`
+	GrossAmount   int              `json:"gross_amount" form:"gross_amount" validate:"required"`
+	DateStart     time.Time        `json:"-" form:"-"`
+	DateEnd       time.Time        `json:"-" form:"-"`
+}
+
+type BookingProduct struct {
+	ID          uint
+	IdBooking   uint
+	IdProduct   uint   `json:"id_product" form:"id_product"`
+	ProductQty  int    `json:"product_qty" form:"product_qty"`
+	ProductName string `json:"product_name" form:"product_name"`
+	RentPrice   int    `json:"rent_price" form:"rent_price"`
+}
+
+type UpdateFormat struct {
+	ID              uint             `json:"id" form:"id"`
+	IdUser          uint             `json:"id_user" form:"id_user"`
+	Start           string           `json:"date_start" form:"date_start"`
+	End             string           `json:"date_end" form:"date_end"`
+	Entrance        string           `json:"entrance" form:"entrance"`
+	Ticket          int              `json:"ticket" form:"ticket"`
+	Product         []BookingProduct `json:"product" form:"product"`
+	IdRanger        uint             `json:"id_ranger" form:"id_ranger"`
+	GrossAmount     int              `json:"gross_amount" form:"gross_amount"`
+	StatusBooking   string           `json:"status" form:"status"`
+	StatusPendakian string           `json:"status_pendakian" form:"status_pendakian"`
+	DateStart       time.Time        `json:"-" form:"-"`
+	DateEnd         time.Time        `json:"-" form:"-"`
+}
+
+type UpdateMidtrans struct {
+	ID            uint   `json:"id" form:"id"`
+	OrderID       string `json:"order_id" form:"order_id"`
+	StatusBooking string `json:"status" form:"status"`
+}
+
+type GetId struct {
+	id uint `param:"id"`
+}
+
+func ToDomain(i interface{}) domain.Core {
+	switch i.(type) {
+	case RegisterFormat:
+		var arr []domain.BookingProductCore
+		cnv := i.(RegisterFormat)
+		for _, val := range cnv.Product {
+			arr = append(arr, domain.BookingProductCore{IdProduct: val.IdProduct, ProductQty: val.ProductQty})
+		}
+		return domain.Core{IdUser: cnv.IdUser, DateStart: cnv.DateStart, DateEnd: cnv.DateEnd, Entrance: cnv.Entrance, Ticket: cnv.Ticket,
+			OrderId: cnv.OrderId, BookingProductCores: arr, IdRanger: cnv.IdRanger, GrossAmount: cnv.GrossAmount, StatusBooking: "unpaid"}
+	case UpdateFormat:
+		var arr []domain.BookingProductCore
+		cnv := i.(UpdateFormat)
+		for _, val := range cnv.Product {
+			arr = append(arr, domain.BookingProductCore{IdProduct: val.IdProduct, ProductQty: val.ProductQty})
+		}
+		return domain.Core{ID: cnv.ID, IdUser: cnv.IdUser, DateStart: cnv.DateStart, DateEnd: cnv.DateEnd, Entrance: cnv.Entrance, Ticket: cnv.Ticket,
+			BookingProductCores: arr, IdRanger: cnv.IdRanger, GrossAmount: cnv.GrossAmount, StatusBooking: cnv.StatusBooking}
+	case UpdateMidtrans:
+		cnv := i.(UpdateMidtrans)
+		return domain.Core{OrderId: cnv.OrderID, StatusBooking: cnv.StatusBooking}
+	}
+	return domain.Core{}
+}
+
+func (i RegisterFormat) ToModel() domain.Core {
+	return domain.Core{
+		IdUser:              i.IdUser,
+		DateStart:           i.DateStart,
+		DateEnd:             i.DateEnd,
+		Entrance:            i.Entrance,
+		Ticket:              i.Ticket,
+		OrderId:             i.OrderId,
+		BookingProductCores: []domain.BookingProductCore{},
+		IdRanger:            i.IdRanger,
+		StatusBooking:       i.StatusBooking,
+		GrossAmount:         i.GrossAmount,
+	}
+}
--- features/booking/delivery/response.go
+++ features/booking/delivery/response.go
@@ -1,1 +1,152 @@
 package delivery
+
+import (
+	"GunungKuy/features/booking/domain"
+	"time"
+
+	"github.com/midtrans/midtrans-go/coreapi"
+)
+
+func SuccessResponse(msg string, data interface{}) map[string]interface{} {
+	return map[string]interface{}{
+		"message": msg,
+		"data":    data,
+	}
+}
+
+func SuccessResponseNoData(msg string) map[string]interface{} {
+	return map[string]interface{}{
+		"message": msg,
+	}
+}
+
+func FailResponse(msg interface{}) map[string]interface{} {
+	return map[string]interface{}{
+		"message": msg,
+	}
+}
+
+type RegisterResponse struct {
+	IdUser      uint             `json:"id_user" form:"id_user"`
+	DateStart   time.Time        `json:"date_start" form:"date_start"`
+	DateEnd     time.Time        `json:"date_end" form:"date_end"`
+	Entrance    string           `json:"entrance" form:"entrance"`
+	Ticket      int              `json:"ticket" form:"ticket"`
+	Product     []BookingProduct `json:"product" form:"product"`
+	IdRanger    uint             `json:"id_ranger" form:"id_ranger"`
+	GrossAmount int              `json:"gross_amount" form:"gross_amount"`
+	OrderId     string           `json:"order_id" form:"order_id"`
+	Link        string           `json:"link" form:"link"`
+}
+
+type UpdateResponse struct {
+	ID            uint             `json:"id" form:"id"`
+	IdUser        uint             `json:"id_user" form:"id_user"`
+	DateStart     time.Time        `json:"date_start" form:"date_start"`
+	DateEnd       time.Time        `json:"date_end" form:"date_end"`
+	Entrance      string           `json:"entrance" form:"entrance"`
+	Ticket        int              `json:"ticket" form:"ticket"`
+	Product       []BookingProduct `json:"product" form:"product"`
+	IdRanger      uint             `json:"id_ranger" form:"id_ranger"`
+	GrossAmount   int              `json:"gross_amount" form:"gross_amount"`
+	OrderId       string           `json:"order_id" form:"order_id"`
+	Link          string           `json:"link" form:"link"`
+	StatusBooking string           `json:"status" form:"status"`
+}
+
+type DetailResponse struct {
+	ID            uint             `json:"id_booking" form:"id_booking"`
+	Start         string           `json:"date_start" form:"date_start"`
+	End           string           `json:"date_end" form:"date_end"`
+	Entrance      string           `json:"entrance" form:"entrance"`
+	Ticket        int              `json:"ticket" form:"ticket"`
+	Product       []BookingProduct `json:"product" form:"product"`
+	IdRanger      uint             `json:"id_ranger" form:"id_ranger"`
+	GrossAmount   int              `json:"gross_amount" form:"gross_amount"`
+	OrderId       string           `json:"order_id" form:"order_id"`
+	Link          string           `json:"link" form:"link"`
+	StatusBooking string           `json:"status" form:"status"`
+	DateStart     time.Time        `json:"-" form:"-"`
+	DateEnd       time.Time        `json:"-" form:"-"`
+}
+
+type GetResponse struct {
+	ID            uint   `json:"id_booking" form:"id_booking"`
+	GrossAmount   int    `json:"gross_amount" form:"gross_amount"`
+	OrderId       string `json:"order_id" form:"order_id"`
+	Link          string `json:"link" form:"link"`
+	StatusBooking string `json:"status" form:"status"`
+}
+
+type GetRangerResponse struct {
+	ID        uint      `json:"id_booking" form:"id_booking"`
+	IdUser    uint      `json:"id_pendaki" form:"id_pendaki"`
+	FullName  string    `json:"fullname" form:"fullname"`
+	Entrance  string    `json:"entrance" form:"entrance"`
+	Start     string    `json:"date_start" form:"date_start"`
+	End       string    `json:"date_end" form:"date_end"`
+	Ticket    int       `json:"ticket" form:"ticket"`
+	DateStart time.Time `json:"-" form:"-"`
+	DateEnd   time.Time `json:"-" form:"-"`
+}
+
+func ToResponse(core interface{}, code string) interface{} {
+	var res interface{}
+	switch code {
+	case "register":
+		cnv := core.(domain.Core)
+		var arr []BookingProduct
+		for _, val := range cnv.BookingProductCores {
+			arr = append(arr, BookingProduct{ID: val.ID, IdBooking: val.IdBooking, IdProduct: val.IdProduct, ProductQty: val.ProductQty})
+		}
+		res = RegisterResponse{IdUser: cnv.IdRanger, DateStart: cnv.DateStart, DateEnd: cnv.DateEnd, Entrance: cnv.Entrance, Ticket: cnv.Ticket,
+			Product: arr, IdRanger: cnv.IdRanger, GrossAmount: cnv.GrossAmount, OrderId: cnv.OrderId, Link: cnv.Link}
+	case "update":
+		cnv := core.(domain.Core)
+		var arr []BookingProduct
+		for _, val := range cnv.BookingProductCores {
+			arr = append(arr, BookingProduct{ID: val.ID, IdBooking: val.IdBooking, IdProduct: val.IdProduct, ProductQty: val.ProductQty})
+		}
+
+		res = UpdateResponse{ID: cnv.ID, IdUser: cnv.IdRanger, DateStart: cnv.DateStart, DateEnd: cnv.DateEnd, Entrance: cnv.Entrance, Ticket: cnv.Ticket,
+			Product: arr, IdRanger: cnv.IdRanger, GrossAmount: cnv.GrossAmount, OrderId: cnv.OrderId, Link: cnv.Link}
+	case "getdetails":
+		cnv := core.(domain.Core)
+		var arr []BookingProduct
+		for _, val := range cnv.BookingProductCores {
+			arr = append(arr, BookingProduct{ID: val.ID, IdBooking: val.IdBooking, IdProduct: val.IdProduct, ProductQty: val.ProductQty,
+				ProductName: val.ProductName, RentPrice: val.RentPrice})
+		}
+		res = DetailResponse{ID: cnv.ID, Start: cnv.DateStart.Format("2006-01-02"), End: cnv.DateEnd.Format("2006-01-02"), Entrance: cnv.Entrance, Ticket: cnv.Ticket,
+			Product: arr, IdRanger: cnv.IdRanger, GrossAmount: cnv.GrossAmount, OrderId: cnv.OrderId, Link: cnv.Link, StatusBooking: cnv.StatusBooking}
+	}
+
+	return res
+}
+
+func ToResponseArray(core interface{}, code string) interface{} {
+	var res interface{}
+	switch code {
+	case "getall":
+		var arr []GetResponse
+		val := core.([]domain.Core)
+		for _, cnv := range val {
+			arr = append(arr, GetResponse{ID: cnv.ID, GrossAmount: cnv.GrossAmount,
+				OrderId: cnv.OrderId, Link: cnv.Link, StatusBooking: cnv.StatusBooking})
+		}
+		res = arr
+
+	case "getranger":
+		var arr []GetRangerResponse
+		val := core.([]domain.Core)
+		for _, cnv := range val {
+			arr = append(arr, GetRangerResponse{ID: cnv.ID, IdUser: cnv.IdUser, FullName: cnv.FullName, Entrance: cnv.Entrance, Start: cnv.DateStart.Format("2006-01-02"), End: cnv.DateEnd.Format("2006-01-02"), Ticket: cnv.Ticket})
+		}
+		res = arr
+	}
+	return res
+}
+
+func ToDomainCheckMidtrans(i *coreapi.TransactionStatusResponse) domain.Core {
+	return domain.Core{OrderId: i.OrderID, StatusBooking: i.TransactionStatus}
+}
