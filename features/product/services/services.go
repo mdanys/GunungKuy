b7aa0293e1bf6d552abc05c8dfaf6f579ef4c1de
mdanys <mochammaddany@gmail.com>
2022-12-07 07:14:13 +0700

    add services.go

--- features/product/services/services.go
+++ features/product/services/services.go
@@ -1,1 +1,36 @@
 package services
+
+import (
+	"GunungKuy/features/product/domain"
+)
+
+type productService struct {
+	qry domain.Repository
+}
+
+func New(repo domain.Repository) domain.Service {
+	return &productService{qry: repo}
+}
+
+// SERVICE TO SHOW ALL PRODUCT
+func (ps *productService) ShowAll(page uint) ([]domain.Core, int, int, error) {
+
+	res, pages, totalPage, err := ps.qry.GetAll(page)
+	if err != nil {
+		return []domain.Core{}, 0, 0, err
+	}
+
+	return res, pages, totalPage, nil
+
+}
+
+// SERVICE TO SHOW PRODUCT DETAIL
+func (ps *productService) ShowByID(id uint) (domain.Core, error) {
+
+	res, err := ps.qry.GetByID(id)
+	if err != nil {
+		return domain.Core{}, err
+	}
+	return res, nil
+
+}
