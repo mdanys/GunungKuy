b74df7b95bbcb6fbe4c8333bec22e30d48f16de0
mdanys <mochammaddany@gmail.com>
2022-12-07 09:02:41 +0700

    add service.go

--- features/climber/services/services.go
+++ features/climber/services/services.go
@@ -1,1 +1,26 @@
 package services
+
+import (
+	"GunungKuy/features/climber/domain"
+	"errors"
+)
+
+type climberService struct {
+	qry domain.Repository
+}
+
+func New(repo domain.Repository) domain.Services {
+	return &climberService{
+		qry: repo,
+	}
+}
+
+// SERVICE TO SHOW CLIMBER DATA
+func (as *climberService) ShowClimber() (domain.Core, error) {
+	res, err := as.qry.GetClimber()
+	if err != nil {
+		return domain.Core{}, errors.New("no data")
+	}
+
+	return res, nil
+}
