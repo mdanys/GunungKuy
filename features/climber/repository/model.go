8411234aecf6151a0d55201489f0cb5b9d625cb8
mdanys <mochammaddany@gmail.com>
2022-12-07 09:01:39 +0700

    add model.go

--- features/climber/repository/model.go
+++ features/climber/repository/model.go
@@ -1,1 +1,23 @@
 package repository
+
+import (
+	"GunungKuy/features/climber/domain"
+
+	"gorm.io/gorm"
+)
+
+type Climber struct {
+	gorm.Model
+	IsClimber     int
+	MaleClimber   int
+	FemaleClimber int
+}
+
+func ToDomain(c Climber) domain.Core {
+	return domain.Core{
+		ID:            c.ID,
+		IsClimber:     c.IsClimber,
+		MaleClimber:   c.MaleClimber,
+		FemaleClimber: c.FemaleClimber,
+	}
+}
