861f5374eccc4b4303769fc4bfeef78e356e65ba
mdanys <mochammaddany@gmail.com>
2022-12-07 09:02:09 +0700

    add query.go

--- features/climber/repository/query.go
+++ features/climber/repository/query.go
@@ -1,1 +1,33 @@
 package repository
+
+import (
+	"GunungKuy/features/climber/domain"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type repoQuery struct {
+	db *gorm.DB
+}
+
+func New(dbConn *gorm.DB) domain.Repository {
+	return &repoQuery{
+		db: dbConn,
+	}
+}
+
+// GET CLIMBER DATA
+func (rq *repoQuery) GetClimber() (domain.Core, error) {
+	var resQryClimber Climber
+
+	result := rq.db.Order("created_at desc").First(&resQryClimber)
+	if result.RowsAffected == 0 {
+		return domain.Core{}, errors.New("not found")
+	}
+
+	// selesai dari DB
+	resClimber := ToDomain(resQryClimber)
+
+	return resClimber, nil
+}
