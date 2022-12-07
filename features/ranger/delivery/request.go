db4a9d0b457b249092526710bc37387365f2d4ba
mdanys <mochammaddany@gmail.com>
2022-12-07 07:09:35 +0700

    add request.go, responses.go, and handler.go

--- features/ranger/delivery/request.go
+++ features/ranger/delivery/request.go
@@ -1,1 +1,36 @@
 package delivery
+
+import (
+	"GunungKuy/features/ranger/domain"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type ApplyFormat struct {
+	Fullname string `json:"fullname" form:"fullname" validate:"required"`
+	Phone    string `json:"phone" form:"phone" validate:"required"`
+	Ttl      string `json:"ttl" form:"ttl" validate:"required"`
+	Gender   string `json:"gender" form:"gender" validate:"required"`
+	Address  string `json:"address" form:"address" validate:"required"`
+	Docs     string `json:"docs" form:"docs"`
+	Price    uint   `json:"price" form:"price"`
+	Detail   string `json:"detail" form:"detail"`
+	UserID   uint   `json:"id_user" form:"id_user"`
+}
+
+type GetFormat struct {
+	StartDate time.Time `validate:"required"`
+	EndDate   time.Time `validate:"required"`
+}
+
+func ToCore(i interface{}) (domain.Core, domain.User) {
+	switch i.(type) {
+	case ApplyFormat:
+		cnv := i.(ApplyFormat)
+		return domain.Core{Docs: cnv.Docs, Price: 100000, Detail: cnv.Detail, UserID: cnv.UserID},
+			domain.User{Model: gorm.Model{ID: cnv.UserID}, FullName: cnv.Fullname, Phone: cnv.Phone, Dob: cnv.Ttl, Gender: cnv.Gender, Address: cnv.Address}
+	}
+
+	return domain.Core{}, domain.User{}
+}
--- features/ranger/delivery/responses.go
+++ features/ranger/delivery/responses.go
@@ -1,1 +1,61 @@
 package delivery
+
+import "GunungKuy/features/ranger/domain"
+
+type ApplyResponse struct {
+	ID          uint        `json:"id_ranger" form:"id_ranger"`
+	User        domain.User `json:"-" form:"-"`
+	IdUser      uint        `json:"id_user" form:"id_user"`
+	Fullname    string      `json:"fullname" form:"fullname"`
+	Ttl         string      `json:"ttl" form:"ttl"`
+	Address     string      `json:"address" form:"address"`
+	Phone       string      `json:"phone" form:"phone"`
+	Gender      string      `json:"gender" form:"gender"`
+	Docs        string      `json:"docs" form:"docs"`
+	Price       uint        `json:"price" form:"price"`
+	Detail      string      `json:"detail" form:"detail"`
+	StatusApply string      `json:"status_apply" form:"status_apply"`
+}
+
+type RangerResponse struct {
+	ID          uint        `json:"id_ranger" form:"id_ranger"`
+	User        domain.User `json:"-" form:"-"`
+	Fullname    string      `json:"fullname" form:"fullname"`
+	Price       uint        `json:"price" form:"price"`
+	Detail      string      `json:"detail" form:"detail"`
+	UserPicture string      `json:"user_picture" form:"user_picture"`
+}
+
+func ToResponse(core interface{}, code string) interface{} {
+	var res interface{}
+
+	switch code {
+	case "apply":
+		cnv := core.(domain.Core)
+		res = ApplyResponse{ID: cnv.ID, IdUser: cnv.User.ID, Fullname: cnv.User.FullName, Ttl: cnv.User.Dob, Address: cnv.User.Address,
+			Phone: cnv.User.Phone, Gender: cnv.User.Gender, Docs: cnv.Docs, Price: cnv.Price, Detail: cnv.Detail, StatusApply: cnv.StatusApply}
+	case "ranger":
+		cnv := core.([]domain.Core)
+		var arr []RangerResponse
+
+		for _, val := range cnv {
+			arr = append(arr, RangerResponse{ID: val.ID, Price: val.Price, Detail: val.Detail, Fullname: val.User.FullName, UserPicture: val.User.UserPicture})
+		}
+		res = arr
+	}
+
+	return res
+}
+
+func SuccessResponse(msg string, data interface{}) map[string]interface{} {
+	return map[string]interface{}{
+		"message": msg,
+		"data":    data,
+	}
+}
+
+func FailResponse(msg string) map[string]interface{} {
+	return map[string]interface{}{
+		"message": msg,
+	}
+}
