6bb64971df578ab741b3fb5164d741a4f5d65e62
mdanys <mochammaddany@gmail.com>
2022-12-07 08:55:16 +0700

    add model.go and query.go

--- features/admin/repository/model.go
+++ features/admin/repository/model.go
@@ -1,1 +1,265 @@
 package repository
+
+import (
+	"GunungKuy/features/admin/domain"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	FullName    string
+	Email       string
+	Password    string
+	Role        string
+	Phone       string
+	Address     string
+	Dob         string
+	Gender      string
+	UserPicture string
+}
+
+type Booking struct {
+	gorm.Model
+	IdUser          uint
+	DateStart       time.Time
+	DateEnd         time.Time
+	Entrance        string
+	Ticket          int
+	IdRanger        uint
+	GrossAmount     int
+	Token           string
+	OrderId         string
+	Link            string
+	StatusBooking   string
+	StatusPendakian string
+	FullName        string `gorm:"-:migration;<-:false"`
+	Phone           string `gorm:"-:migration;<-:false"`
+}
+
+type Pesan struct {
+	gorm.Model
+	IdRanger uint
+	Email    string
+	Status   string
+}
+
+type Product struct {
+	gorm.Model
+	ProductName    string
+	RentPrice      int
+	Detail         string
+	Note           string
+	ProductPicture string
+}
+
+type Ranger struct {
+	gorm.Model
+	UserID      uint
+	Docs        string
+	Price       uint
+	Detail      string
+	Status      string
+	StatusApply string
+	User        User
+}
+
+type Climber struct {
+	gorm.Model
+	IsClimber     int
+	MaleClimber   int
+	FemaleClimber int
+}
+
+type Code struct {
+	gorm.Model
+	Code         string
+	AccessToken  string
+	TokenType    string
+	RefreshToken string
+	Expiry       time.Time
+}
+
+func FromDomainClimber(db domain.ClimberCore) Climber {
+	return Climber{
+		Model:         gorm.Model{ID: db.ID},
+		IsClimber:     db.IsClimber,
+		MaleClimber:   db.MaleClimber,
+		FemaleClimber: db.FemaleClimber,
+	}
+}
+
+func FromDomainProduct(db domain.ProductCore) Product {
+	return Product{
+		Model:          gorm.Model{ID: db.ID},
+		ProductName:    db.ProductName,
+		RentPrice:      db.RentPrice,
+		Detail:         db.Detail,
+		Note:           db.Note,
+		ProductPicture: db.ProductPicture,
+	}
+}
+
+func FromDomainBooking(db domain.BookingCore) Booking {
+	return Booking{
+		Model:           gorm.Model{ID: db.ID},
+		IdUser:          db.IdUser,
+		DateStart:       db.DateStart,
+		DateEnd:         db.DateEnd,
+		Entrance:        db.Entrance,
+		Ticket:          db.Ticket,
+		IdRanger:        db.IdRanger,
+		GrossAmount:     db.GrossAmount,
+		Token:           db.Token,
+		OrderId:         db.OrderId,
+		Link:            db.Link,
+		StatusBooking:   db.StatusBooking,
+		StatusPendakian: db.StatusPendakian,
+	}
+}
+
+func ToDomainCode(dc Code) domain.Code {
+	return domain.Code{
+		ID:           dc.ID,
+		Code:         dc.Code,
+		AccessToken:  dc.AccessToken,
+		TokenType:    dc.TokenType,
+		RefreshToken: dc.RefreshToken,
+		Expiry:       dc.Expiry,
+	}
+}
+
+func ToDomainPesan(dc Pesan) domain.PesanCore {
+	return domain.PesanCore{
+		ID:       dc.ID,
+		IdRanger: dc.IdRanger,
+		Email:    dc.Email,
+		Status:   dc.Status,
+	}
+}
+
+func ToDomainClimber(db Climber) domain.ClimberCore {
+	return domain.ClimberCore{
+		ID:            db.ID,
+		IsClimber:     db.IsClimber,
+		MaleClimber:   db.MaleClimber,
+		FemaleClimber: db.FemaleClimber,
+	}
+}
+
+func ToDomainProduct(db Product) domain.ProductCore {
+	return domain.ProductCore{
+		ID:             db.ID,
+		ProductName:    db.ProductName,
+		RentPrice:      db.RentPrice,
+		Detail:         db.Detail,
+		Note:           db.Note,
+		ProductPicture: db.ProductPicture,
+	}
+}
+
+func ToDomainBooking(db []Booking) []domain.BookingCore {
+	var arr []domain.BookingCore
+	for _, val := range db {
+		arr = append(arr, domain.BookingCore{
+			ID:              val.ID,
+			IdUser:          val.IdUser,
+			DateStart:       val.DateStart,
+			DateEnd:         val.DateEnd,
+			Entrance:        val.Entrance,
+			Ticket:          val.Ticket,
+			IdRanger:        val.IdRanger,
+			GrossAmount:     val.GrossAmount,
+			Token:           val.Token,
+			OrderId:         val.OrderId,
+			Link:            val.Link,
+			StatusBooking:   val.StatusBooking,
+			StatusPendakian: val.StatusPendakian,
+			FullName:        val.FullName,
+			Phone:           val.Phone,
+		})
+	}
+	return arr
+}
+
+func ToDomainProductArr(db []Product) []domain.ProductCore {
+	var arr []domain.ProductCore
+	for _, val := range db {
+		arr = append(arr, domain.ProductCore{
+			ID:             val.ID,
+			ProductName:    val.ProductName,
+			RentPrice:      val.RentPrice,
+			Detail:         val.Detail,
+			Note:           val.Note,
+			ProductPicture: val.ProductPicture,
+		})
+	}
+	return arr
+}
+
+func FromDomainPesan(mail string, dr Ranger) Pesan {
+	return Pesan{
+		Model:    gorm.Model{ID: dr.ID},
+		IdRanger: dr.ID,
+		Email:    mail,
+		Status:   dr.StatusApply,
+	}
+}
+
+func FromDomainRanger(dr domain.RangerCore) Ranger {
+	return Ranger{
+		Model:       gorm.Model{ID: dr.ID},
+		UserID:      dr.UserID,
+		Docs:        dr.Docs,
+		Price:       dr.Price,
+		Detail:      dr.Detail,
+		Status:      dr.Status,
+		StatusApply: dr.StatusApply,
+		User:        User{Phone: dr.User.Phone},
+	}
+}
+
+func ToDomainRanger(r Ranger) domain.RangerCore {
+	return domain.RangerCore{
+		ID:          r.ID,
+		UserID:      r.UserID,
+		Docs:        r.Docs,
+		Price:       r.Price,
+		Detail:      r.Detail,
+		Status:      r.Status,
+		StatusApply: r.StatusApply,
+		User:        domain.UserCore{Phone: r.User.Phone},
+	}
+}
+
+func ToDomainRangerArray(ar []Ranger) []domain.RangerCore {
+	var arr []domain.RangerCore
+	for _, val := range ar {
+		arr = append(arr, domain.RangerCore{
+			ID:          val.ID,
+			UserID:      val.UserID,
+			Docs:        val.Docs,
+			Price:       val.Price,
+			Detail:      val.Detail,
+			Status:      val.Status,
+			StatusApply: val.StatusApply,
+			User:        domain.UserCore{FullName: val.User.FullName, Address: val.User.Address, Phone: val.User.Phone, Gender: val.User.Gender},
+		})
+	}
+	return arr
+}
+
+func FromDomainUser(du domain.UserCore) User {
+	return User{
+		Model: gorm.Model{ID: (du.ID)},
+		Phone: du.Phone,
+	}
+}
+
+func ToDomainUser(u User) domain.UserCore {
+	return domain.UserCore{
+		ID:    (u.ID),
+		Phone: u.Phone,
+	}
+}
--- features/admin/repository/query.go
+++ features/admin/repository/query.go
@@ -1,1 +1,203 @@
 package repository
+
+import (
+	"GunungKuy/features/admin/domain"
+	"errors"
+	"time"
+
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+type repoQuery struct {
+	db *gorm.DB
+}
+
+func New(dbConn *gorm.DB) domain.Repository {
+	return &repoQuery{
+		db: dbConn,
+	}
+}
+
+// TAKE DATA PENDAKI
+func (rq *repoQuery) GetPendaki() ([]domain.BookingCore, domain.ClimberCore, error) {
+	var resQry []Booking
+	var resQryClimber Climber
+	now := time.Now().Format("2006-01-02")
+	start, _ := time.Parse("2006-01-02", now)
+	end := start.AddDate(0, 0, 14)
+
+	if err := rq.db.Order("created_at desc").First(&resQryClimber).Error; err != nil {
+		return nil, domain.ClimberCore{}, errors.New("cannot get climber data")
+	}
+
+	if err := rq.db.Select("bookings.id_user", "users.full_name", "bookings.entrance", "bookings.date_start", "bookings.date_end").
+		Order("bookings.date_start asc").Joins("left join users on users.id = bookings.id_user").
+		Where("bookings.date_start BETWEEN ? AND ?", start, end).
+		Find(&resQry).Scan(&resQry).Error; err != nil {
+		return nil, domain.ClimberCore{}, err
+	}
+
+	// selesai dari DB
+	res := ToDomainBooking(resQry)
+	resClimber := ToDomainClimber(resQryClimber)
+
+	return res, resClimber, nil
+}
+
+// ADD DATA CLIMBER
+func (rq *repoQuery) InsertClimber(data domain.ClimberCore) (domain.ClimberCore, error) {
+	var resQry Climber = FromDomainClimber(data)
+	if err := rq.db.Create(&resQry).Error; err != nil {
+		return domain.ClimberCore{}, err
+	}
+
+	data = ToDomainClimber(resQry)
+	return data, nil
+}
+
+// GET PRODUCT DATA
+func (rq *repoQuery) GetProduct(page int) ([]domain.ProductCore, int, int, error) {
+	var resQry []Product
+	var sum float64
+	var totalPage int64
+
+	if page == 0 || page == 1 {
+		page = 1
+		if err := rq.db.Order("created_at desc").Limit(8).Find(&resQry).Error; err != nil {
+			return nil, 0, 0, errors.New("no data")
+		}
+	} else {
+		i := (page - 1) * 8
+		if err := rq.db.Order("created_at desc").Offset(i).Limit(8).Find(&resQry).Scan(&resQry).Error; err != nil {
+			return nil, 0, 0, errors.New("no data")
+		}
+	}
+
+	rq.db.Model(&Product{}).Count(&totalPage)
+	sum = float64(totalPage) / 8
+	if sum > float64((int(sum))) {
+		totalPage = int64(sum) + 1
+	} else if sum == float64((int(sum))) {
+		totalPage = 1
+	} else if sum > 0 {
+		totalPage = 1
+	}
+
+	if page > int(totalPage) {
+		return nil, 0, 0, errors.New("page not found")
+	}
+
+	res := ToDomainProductArr(resQry)
+	return res, page, int(totalPage), nil
+}
+
+// ADD PRODUCT
+func (rq *repoQuery) InsertProduct(newProduct domain.ProductCore) (domain.ProductCore, error) {
+	var res Product = FromDomainProduct(newProduct)
+	if err := rq.db.Create(&res).Error; err != nil {
+		return domain.ProductCore{}, err
+	}
+
+	newProduct = ToDomainProduct(res)
+	return newProduct, nil
+}
+
+// UPDATE PRODUCT
+func (rq *repoQuery) UpdateProduct(newProduct domain.ProductCore) (domain.ProductCore, error) {
+	var res Product = FromDomainProduct(newProduct)
+	err := rq.db.Where("id=?", newProduct.ID).Updates(&res)
+	if err.RowsAffected == 0 {
+		return domain.ProductCore{}, errors.New("no data")
+	}
+
+	newProduct = ToDomainProduct(res)
+	return newProduct, nil
+}
+
+// DELETE PRODUCT
+func (rq *repoQuery) DeleteProduct(id int) error {
+	err := rq.db.Where("id=?", id).Delete(&Product{})
+	if err.RowsAffected == 0 {
+		return errors.New("no data")
+	}
+	return nil
+}
+
+// GET DATA RANGER
+func (rq *repoQuery) GetAllRanger() ([]domain.RangerCore, []domain.RangerCore, error) {
+	var data []Ranger
+	var datas []Ranger
+
+	if err := rq.db.Preload("User").Where("status_apply = ?", "accepted").Find(&data).Error; err != nil {
+		log.Error("error on get all ranger", err.Error())
+		return nil, nil, err
+	}
+
+	if err := rq.db.Preload("User").Where("status_apply = ?", "waiting").Find(&datas).Error; err != nil {
+		log.Error("error on get all applied ranger", err.Error())
+		return nil, nil, err
+	}
+
+	resAccepted := ToDomainRangerArray(data)
+	res := ToDomainRangerArray(datas)
+	return resAccepted, res, nil
+}
+
+// EDIT DATA RANGER
+func (rq *repoQuery) EditRanger(data domain.RangerCore, datas domain.UserCore, id uint) (domain.RangerCore, domain.UserCore, domain.PesanCore, error) {
+	var cnv Ranger = FromDomainRanger(data)
+	var conv User = FromDomainUser(datas)
+	var pesan domain.PesanCore
+	var mail string
+
+	if err := rq.db.Table("rangers").Where("id = ?", id).Updates(&cnv).Error; err != nil {
+		log.Error("error on edit status apply ranger", err.Error())
+		return domain.RangerCore{}, domain.UserCore{}, domain.PesanCore{}, err
+	}
+
+	if err := rq.db.Preload("User").Table("rangers").Where("id = ?", id).First(&cnv).Error; err != nil {
+		log.Error("error on getting after edit", err.Error())
+		return domain.RangerCore{}, domain.UserCore{}, domain.PesanCore{}, err
+	}
+
+	if data.StatusApply != "" {
+		rq.db.Model(&User{}).Where("id = ?", cnv.UserID).Select("email").Find(&mail)
+		var pes Pesan = FromDomainPesan(mail, cnv)
+		rq.db.Save(&pes)
+		pesan = ToDomainPesan(pes)
+	}
+
+	if err := rq.db.Table("users").Where("id = ?", cnv.UserID).Updates(&conv).Error; err != nil {
+		log.Error("error on edit phone user", err.Error())
+		return domain.RangerCore{}, domain.UserCore{}, domain.PesanCore{}, err
+	}
+
+	if err := rq.db.Table("users").Where("id = ?", cnv.UserID).Update("role", "ranger").Error; err != nil {
+		log.Error("error on edit role user", err.Error())
+		return domain.RangerCore{}, domain.UserCore{}, domain.PesanCore{}, err
+	}
+
+	res := ToDomainRanger(cnv)
+	resU := ToDomainUser(conv)
+	return res, resU, pesan, nil
+}
+
+// DELETE DATA RANGER
+func (rq *repoQuery) DeleteRanger(id int) error {
+	err := rq.db.Where("id=?", id).Delete(&Ranger{})
+	if err.RowsAffected == 0 {
+		return errors.New("no data")
+	}
+	return nil
+}
+
+// GET OAUTH TOKEN
+func (rq *repoQuery) GetCode() (domain.Code, error) {
+	var resQry Code
+	if err := rq.db.Order("created_at desc").First(&resQry).Error; err != nil {
+		return domain.Code{}, err
+	}
+	res := ToDomainCode(resQry)
+	return res, nil
+}
