34309c4724862dfeca34a81befeddcf61019fccc
mdanys <mochammaddany@gmail.com>
2022-12-07 08:50:39 +0700

    add request.go, response.go and handler.go

--- features/google/delivery/request.go
+++ features/google/delivery/request.go
@@ -1,1 +1,26 @@
 package delivery
+
+import (
+	"GunungKuy/features/google/domain"
+
+	"golang.org/x/oauth2"
+)
+
+type RegisterFormat struct {
+	Code string `json:"code" form:"code"`
+}
+
+type Pesan struct {
+	Email  string `json:"email" form:"email"`
+	Status string `json:"status" form:"status"`
+}
+
+type UserInfoFormat struct {
+	Email        string `json:"email"`
+	Fullname     string `json:"name"`
+	Photoprofile string `json:"picture"`
+}
+
+func ToDomain(core *oauth2.Token, code string) domain.Code {
+	return domain.Code{Code: code, AccessToken: core.AccessToken, TokenType: core.TokenType, RefreshToken: core.RefreshToken, Expiry: core.Expiry}
+}
